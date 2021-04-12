67d2d2eb44d5f0d182daf213ffd95d5c933d380b
3imed-jaberi <imed_jebari@hotmail.fr>
2021-04-12 23:59:39 +0200

    add the wrapper src code 🚀 ..

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-# init 🚧 ..
+/*!
+ * koa-better-error-handler-wrapper
+ * Copyright(c) 2021 Imed Jaberi
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const koaBetterErrorHandler = require('koa-better-error-handler');
+const koa404Handler = require('koa-404-handler');
+
+/**
+ * Expose `koaBetterErrorHandlerWrapper()`.
+ */
+
+module.exports = koaBetterErrorHandlerWrapper;
+
+/**
+ * A wrapper function provide a fast way to start with koa better error handler.
+ *
+ * @param {KoaApplication} [app]                koa application instance.
+ * @param {Boolean} [isApi=true]                tell the error handler that it's dealing with an API.
+ * @param {Array} [koaBetterErrorHandlerArgs]   all params passed to koa-better-error-handler function.
+ *
+ * @api public
+ * */
+function koaBetterErrorHandlerWrapper(
+  app,
+  isApi = true,
+  ...koaBetterErrorHandlerArgs
+) {
+  // override koa's undocumented error handler.
+  app.context.addEventListener(
+    'error',
+    koaBetterErrorHandler(...koaBetterErrorHandlerArgs)
+  );
+  // specify that this is our api.
+  if (isApi) app.context.api = isApi;
+  // use koa-404-handler.
+  app.use(koa404Handler);
+
+  // return the application
+  return app;
+}
